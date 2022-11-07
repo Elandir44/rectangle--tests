e382879d5c14587696ec2c1347c6486c4a2022da
Elandir44 <101931026+Elandir44@users.noreply.github.com>
2022-11-07 21:03:50 +0100

    Update utils.test.js
     e.g.

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,1 +1,9 @@
 import { generateText, checkAndGenerate} from "../js/utils";
+
+test('description', () => {
+  /*  e.g.
+  const outputText = generateText ('Rainer', 27);
+    expect  (outputText).toBe('Rainer (27 years old)');
+*/
+});
+
